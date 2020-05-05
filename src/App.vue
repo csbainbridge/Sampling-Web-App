260cb210480c3421ace02c2065757363fc737735
Bainbridge <curtis.bainbridge@croda.com>
2020-05-05 17:09:27 +0100

    Initial Commit

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,29 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <b-navbar class="header__navigation">
+          <h1 class="header-nav__title">Sampling Replenishment</h1>
+          <router-link to="/">Home</router-link>
+          <router-link to="/about">About</router-link>
+          <p class="header-nav__branding">Croda</p>
+    </b-navbar>
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
+@import 'node_modules/bootstrap/scss/bootstrap';
+@import 'node_modules/bootstrap-vue/src/index.scss';
+
+$color-theme-dark: #003932;
+$color-theme-light: #078438;
+$color-font-primary: #ffffff;
+$color-background: whitesmoke;
+
+$size-font-standard: 2rem;
+$size-font-large: $size-font-standard * 1.5;
+$size-font-small: $size-font-standard * 0.5;
+$size-font-x-small: $size-font-standard  * 0.25;
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -17,16 +32,8 @@
   color: #2c3e50;
 }
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+.header__navigation {
+  background-color: $color-theme-dark;
+  height: 2em;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import { BootstrapVue, IconsPlugin, TablePlugin } from 'bootstrap-vue'
+
+// Register Bootstrap
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+Vue.use(TablePlugin)
 
 Vue.config.productionTip = false
 
