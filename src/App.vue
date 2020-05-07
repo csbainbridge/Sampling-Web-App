0451946e0126f11d7af206af6a243c7a84d67a86
Bainbridge <curtis.bainbridge@croda.com>
2020-05-07 12:35:08 +0100

    Added header component

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,112 @@
 <template>
   <div id="app">
-    <b-navbar class="header__navigation">
-          <h1 class="header-nav__title">Sampling Replenishment</h1>
-          <router-link to="/">Home</router-link>
-          <router-link to="/about">About</router-link>
-          <p class="header-nav__branding">Croda</p>
-    </b-navbar>
-    <router-view/>
+    <TheHeader></TheHeader>
+    <!-- <router-view/> -->
   </div>
 </template>
+
+<script>
+import TheHeader from '@/components/TheHeader.vue'
+
+export default {
+  components: {
+    TheHeader
+  },
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
 
 <style lang="scss">
 @import 'node_modules/bootstrap/scss/bootstrap';
 @import 'node_modules/bootstrap-vue/src/index.scss';
-
+@import url('https://fonts.googleapis.com/css2?family=IBM+Plex+Serif&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&display=swap');
+/*
+Global Color Theme Config
+*/
 $color-theme-dark: #003932;
-$color-theme-light: #078438;
+$color-theme-light: #008032;
 $color-font-primary: #ffffff;
 $color-background: whitesmoke;
-
-$size-font-standard: 2rem;
-$size-font-large: $size-font-standard * 1.5;
-$size-font-small: $size-font-standard * 0.5;
-$size-font-x-small: $size-font-standard  * 0.25;
-
+/*
+Global Font Config
+Standard 16px
+Large  48px
+Small 12px
+Extra Small 8px
+*/
+$size-font-standard: 1rem;
+$size-font-large: $size-font-standard * 3;
+$size-font-medium: $size-font-standard * 2;
+$size-font-small: $size-font-standard * 0.75;
+$size-font-x-small: $size-font-standard  * 0.5;
+$font-family-default: 'Open Sans', sans-serif;;
+$font-family-brand: 'IBM Plex Serif', serif;
+/*
+Spacing Config
+*/
+$spacing-standard: calc(1rem * 1.25);
+$spacing-small: calc(1rem / 2);
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: $font-family-default;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: $color-font-primary;
 }
-
-.header__navigation {
+h1 {
+  font-size: $size-font-medium * 0.75;
+}
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
   background-color: $color-theme-dark;
-  height: 2em;
+  height: 3rem;
+  white-space: nowrap;
+  border-bottom: 3px solid $color-theme-light;
+}
+.header__app-branding {
+  margin-left: calc(1rem * 1.25); /* 20px */
+  margin-bottom: 0;
+  letter-spacing: 0.2rem;
+}
+.header__right-wrapper {
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+    * {
+      margin: 0;
+    }
+
+    .header__login-wrapper {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      padding: 0 $spacing-standard 0 $spacing-standard;
+      border-right: .75px solid #fff;
+      border-left: .75px solid #fff;
+    }
+
+    .header__icon {
+      margin-right: $spacing-small;
+    }
+
+    .header__company {
+      margin: 0 calc(1rem * 1.25) 0 calc(1rem * 1.25);
+      font-family: $font-family-brand;
+      border-bottom: 1px solid #fff;
+      letter-spacing: 0.1rem;
+      line-height: 2rem;
+    }
+    .header__username {
+      margin-right: $spacing-small;
+    }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,12 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import { BootstrapVue, IconsPlugin, TablePlugin } from 'bootstrap-vue'
+import { BootstrapVue, IconsPlugin, TablePlugin, BootstrapVueIcons } from 'bootstrap-vue'
 
 // Register Bootstrap
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
 Vue.use(TablePlugin)
+Vue.use(BootstrapVueIcons)
 
 Vue.config.productionTip = false
 
